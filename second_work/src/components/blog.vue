9eda5fe02a1c10ece5fcbecd9a28f85995a8a636
dydwang <3010136674@qq.com>
2019-05-02 12:09:57 +0800

    第一次测试提交

---
+++ second_work/src/components/blog.vue
@@ -0,0 +1,70 @@
+//连接数据库测试
+
+
+<template>
+    <div class="">
+        <el-input v-model="inpContent"></el-input>
+        <el-button type="primary" @click="getValue">获取数据</el-button>
+        <el-button type="primary" @click="setValue">添加数据</el-button>
+        <el-button type="primary" @click="getUsername">获用户</el-button>
+        <el-button type="primary" @click="setApply">写入</el-button>
+
+        {{res.data}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'blog',
+        data() {
+            return {
+                inpContent: '',
+                res:[],
+                num:1
+            }
+        },
+        methods: {
+            setApply(){
+                console.error("验证通过")
+                this.$ajax.post('/api/setValue', {
+                     id: 12345,name:"wwww"
+                }).then( (res) => {
+                    console.log('res', res);
+
+                })
+            },
+            getUsername() {
+                let that=this
+                // axios.get('/', {params: ''})
+                this.$ajax.get('/api/getUsername', {
+                    params: {username: "admin"}
+                }).then( (res) => {
+                    that.res=res
+                    console.log('res', this.res);
+                    console.log('res.data[0].name=', this.res.data[0].name);
+                    console.log('res+++++++++++++++++++++', that.inpContent);
+                })
+            },
+            getValue() {
+                let that=this
+                // axios.get('/', {params: ''})
+                this.$ajax.get('/api/getValue', {
+                    params: {id: 2}
+                }).then( (res) => {
+                    that.res=res
+                    console.log('res', this.res);
+                    console.log('res.data[0].name=', this.res.data[0].name);
+                    console.log('res+++++++++++++++++++++', that.inpContent);
+                })
+            },
+            setValue() {
+                // axios.post('/', {})
+                this.$ajax.post('/api/setValue', {
+                    id: 2, name: this.inpContent
+                }).then( (res) => {
+                    console.log('res', res);
+                })
+            }
+        }
+    }
+</script>
---
+++ second_work/src/components/schoolTitle.vue
@@ -0,0 +1,29 @@
+<template>
+    <div style="width: 101.2%;height: 90px;border: 1px solid black;
+                opacity:0.9;margin: 0 -10px;">
+        <div style="margin: 5px 0">
+            <img src="@/assets/GIT.png"
+                style="width: 80px;height: 80px;
+                    margin-left: 50px;">
+            <div style="width: 392px;height: 80px; border: 1px solid;margin-left: 10px">
+                <span style="font-size:28px;color: #000;">贵州理工学院二次排课管理系统</span>
+                <span style="font-size:22px;font-style:oblique;color: #667686;
+                     margin-left: 40px;margin-top: 5px          ">
+                    Guizhou Institute of Technology</span>
+            </div>
+
+            <img src="@/assets/GIThead.png" style="width: 80px;height: 80px;border-radius:40px;
+            border: 1px black solid;margin-left: 666px ">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "schoolTitle"
+    }
+</script>
+
+<style scoped>
+    *{float: left;}
+</style>'
